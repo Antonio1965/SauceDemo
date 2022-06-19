f04a7d0b9180576a93617c924ce1faf749fc9eb0
TSUTSUMI José Antonio <64294956+Antonio1965@users.noreply.github.com>
2022-06-19 13:09:57 -0300

    Mi primer commit

---
+++ src/test/java/Edit/SauceDemo/AppTest.java
@@ -0,0 +1,15 @@
+package Edit.SauceDemo;
+
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	@Test
+	public void lab1_test() {
+		System.out.println("Hola Mundo de Automatizacion !!!!!!");
+	}
+}
